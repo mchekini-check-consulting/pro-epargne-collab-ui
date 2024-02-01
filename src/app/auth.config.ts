4f963b8c83f37a0f34205623b37cbdb49f3b238c
mchekini-check-consulting <115934988+mchekini-check-consulting@users.noreply.github.com>
2024-02-02 00:31:17 +0100

    Update Authentication Configuration

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -2,11 +2,11 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 
 
 export const authCodeFlowConfig: AuthConfig = {
-    issuer: 'https://keycloak.check-consulting.net/auth/realms/master',
+    issuer: 'https://keycloak.check-consulting.net/auth/realms/Pro-epargne-collab',
     redirectUri: window.location.origin,
-    clientId: 'pro-epargne',
+    clientId: 'pro-epargne-collab',
     responseType: 'code',
-    logoutUrl : 'https://keycloak.check-consulting.net/auth/realms/master/protocol/openid-connect/logout',
+    logoutUrl : 'https://keycloak.check-consulting.net/auth/realms/Pro-epargne-collab/protocol/openid-connect/logout',
     postLogoutRedirectUri: window.location.origin,
     showDebugInformation: true,
     requireHttps: false
