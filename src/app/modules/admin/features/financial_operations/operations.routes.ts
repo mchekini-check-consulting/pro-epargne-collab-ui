2536d73a811c1d1b29187ac2edc0af46d372a878
Amoura mohand <amoura.md.rachid@gmail.com>
2024-02-07 23:49:09 +0100

    feat(history): add components transaction history

--- src/app/modules/admin/features/financial_operations/operations.routes.ts
+++ src/app/modules/admin/features/financial_operations/operations.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import {OperationsComponent} from "./operations.component";
 import {FinancialOperationComponent} from "./container/financial-operation/financial-operation.component";
+import {TableOverviewExample} from "./container/financial-operation/component/financial-operation-list";
 
 export default [
     {
         path     : '',
         component: FinancialOperationComponent,
+    },
+    {
+        path     : 'list',
+        component: TableOverviewExample,
     },
 ] as Routes;
