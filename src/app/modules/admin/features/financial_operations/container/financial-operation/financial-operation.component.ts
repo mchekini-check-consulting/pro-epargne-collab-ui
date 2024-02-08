bd1e8c40eaaa3c9308e5ccc8b207c743c713a8eb
sskarim85@gmail.com <sskarim85@gmail.com>
2024-02-08 10:22:01 +0100

    display collaborator accounts

--- src/app/modules/admin/features/financial_operations/container/financial-operation/financial-operation.component.ts
+++ src/app/modules/admin/features/financial_operations/container/financial-operation/financial-operation.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, AfterViewChecked, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from "@angular/material/button";
 import {MatDialog} from "@angular/material/dialog";
 import {TransactionDialogComponent} from "../../tranaction-dialog/transaction-dialog.component";
 import {MatCardModule} from "@angular/material/card";
 import {MatIconModule} from "@angular/material/icon";
+import { CollaboratorAccountService } from 'app/core/service/collaborator-account.service';
+import { CollaboratorAccount } from 'app/core/model/collaborator_account.model';
 
 @Component({
   selector: 'app-financial-operation',
@@ -13,9 +15,13 @@ import {MatIconModule} from "@angular/material/icon";
   templateUrl: './financial-operation.component.html',
   styleUrl: './financial-operation.component.css'
 })
-export class FinancialOperationComponent {
+export class FinancialOperationComponent implements OnInit{
 
-    constructor(public dialog: MatDialog) {}
+    accounts: CollaboratorAccount[] = []
+    totalAmont:number = 0
+
+    constructor(public dialog: MatDialog, private accountService:CollaboratorAccountService) {}
+  
 
     openTransactionDialog() {
         this.dialog.open(TransactionDialogComponent, {
@@ -28,4 +34,17 @@ export class FinancialOperationComponent {
     cardTest() {
         console.log("Test click on card")
     }
+
+    ngOnInit(){
+      
+      this.accountService.getCollaboratorAccounts().subscribe(response=>{
+     
+        if(response.hasOwnProperty("data")){
+          
+          this.accounts = response['data']
+          this.accounts.forEach(account=>this.totalAmont+=account.amount)
+        }
+      })
+    }
+    
 }
