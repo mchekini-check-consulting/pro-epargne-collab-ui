7026abec046e802cf09829381eb59ba564d385c1
dihia-lyz <dihia.lyazid97@gmail.com>
2024-02-16 22:12:15 +0100

    Add simulator feature

--- src/app/modules/admin/features/assets-history/components/assets-history-table/assets-history-table.component.ts
+++ src/app/modules/admin/features/assets-history/components/assets-history-table/assets-history-table.component.ts
@@ -1,27 +1,33 @@
-import {Component, OnInit} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {AssetsHistoryService} from "../../../../../../core/service/assets-history.service";
-import {MatTableDataSource, MatTableModule} from "@angular/material/table";
-import {keys} from "lodash-es";
-import {AssetsDisplay} from "../../../../../../core/model/assetsDisplay.model";
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AssetsHistoryService } from '../../../../../../core/service/assets-history.service';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { keys } from 'lodash-es';
+import { AssetsDisplay } from '../../../../../../core/model/assetsDisplay.model';
+import { MatDialog } from '@angular/material/dialog';
+import { SimulatorComponent } from '../simulator/simulator.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
     selector: 'app-assets-history-table',
     standalone: true,
-    imports: [CommonModule, MatTableModule],
+    imports: [CommonModule, MatTableModule, MatButtonModule],
     templateUrl: './assets-history-table.component.html',
     styleUrl: './assets-history-table.component.scss',
+    encapsulation: ViewEncapsulation.None,
 })
 export class AssetsHistoryTableComponent implements OnInit {
-
+    
     displayedColumns: string[] = ["plans", "supportLabel", "label", "sri"];
     displayedColumnsHeader: string[] = ["Plans", "Libellé du support", "Label", "SRI"];
     years: string[];
     dataSource = new MatTableDataSource<any>([]);
     assets: AssetsDisplay[];
 
-    constructor(private assetsHistory: AssetsHistoryService) {
-    }
+    constructor(
+        private assetsHistory: AssetsHistoryService,
+        private dialog: MatDialog
+    ) {}
 
     ngOnInit(): void {
         this.assetsHistory.getAssetsHistory().subscribe(response => {
@@ -58,5 +64,11 @@ export class AssetsHistoryTableComponent implements OnInit {
             listOfTransformedAssets.push(transformedData);
         }
         return listOfTransformedAssets;
+    }
+
+    showSimulatorDialog(): void {
+        this.dialog.open(SimulatorComponent, {
+            disableClose: true,
+        });
     }
 }
--- src/app/modules/admin/features/assets-history/container/assets-history.component.ts
+++ src/app/modules/admin/features/assets-history/container/assets-history.component.ts
@@ -2,12 +2,18 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AssetsHistoryTableComponent } from '../components/assets-history-table/assets-history-table.component';
 import { SimulatorComponent } from '../components/simulator/simulator.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
     selector: 'app-assets-history',
     standalone: true,
     templateUrl: './assets-history.component.html',
     styleUrl: './assets-history.component.scss',
-    imports: [CommonModule, AssetsHistoryTableComponent, SimulatorComponent],
+    imports: [
+        CommonModule,
+        AssetsHistoryTableComponent,
+        SimulatorComponent,
+        MatButtonModule,
+    ],
 })
 export class AssetsHistoryComponent {}
